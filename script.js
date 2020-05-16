29aeaa255f5f475ed3e8a6cbcf2e293fc24be552
sharman13 <58657244+sharman13@users.noreply.github.com>
2020-05-16 19:37:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
 var click = 0;
+var image = 0;
+var imageprev = 0;
 function Menu(){
     if(window.click == 0){
+        // document.getElementById('nav-mobo').className = "animated slideInDown";
+        document.getElementById('nav-mobo').style.animationName = "wipeIn";
+        document.getElementById('nav-mobo').style.animationDuration = "0.5s";
         document.getElementById('nav-mobo').style.display = "block";
+        
+        // document.getElementsByClassName('slider')[0].style.display = "none";
         window.click = 1;
     }
     else{
-        document.getElementById('nav-mobo').style.display = "none";
-        console.log();
+        document.getElementById('nav-mobo').style.animationName = "wipeOut";
+        document.getElementById('nav-mobo').style.animationDuration = "0.5s";
+        // document.getElementById('nav-mobo').style.display = "none";
         window.click = 0;
     }
 }
-
-function register() {
-    window.scrollBy(0, 500);
+function next(){
+    if(image == 0){
+    document.getElementById("img1").style.display = "none";
+    document.getElementById("img2").style.display = "block";
+    imageprev = 1;
+    image = 1;
+    }
+    else{
+        document.getElementById("img2").style.display = "none";
+        document.getElementById("img1").style.display = "block";
+        image = 0;
+        imageprev = 0;
+    }
 }
+function Prev(){
+    console.log("hello");
+    if(imageprev == 0){
+        document.getElementById("img1").style.display = "none";
+        document.getElementById("img2").style.display = "block";
+        imageprev = 1;
+        }
+        else{
+            document.getElementById("img2").style.display = "none";
+            document.getElementById("img1").style.display = "block";
+            imageprev = 0;
+        }
+}
+
